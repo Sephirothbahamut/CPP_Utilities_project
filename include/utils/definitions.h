3e94422b5274a07d7fe45052c845481089ba9b0c
Sephirothbahamut <sephirothbahamut@live.it>
2021-04-06 14:25:28 +0200

    Fixed the useless utils_discard_anyway to work multiple times in the same scope

--- include/utils/definitions.h
+++ include/utils/definitions.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#define utils_CONCAT(a, b) CONCAT_INNER(a, b)
+#define utils_CONCAT_INNER(a, b) a ## b
+#define utils_discard_anyway auto CONCAT(i_dont_really_care_about_you_and_your_nodiscard_policies_, __LINE__) =
+
 namespace utils
 	{
 	namespace definitions
