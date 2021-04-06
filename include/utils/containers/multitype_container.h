99665e9e251085612de661820dcdbbfc3f2b3af0
Sephirothbahamut <sephirothbahamut@live.it>
2021-04-06 14:30:06 +0200

    Added multitype container and polymorphic container

---
+++ include/utils/containers/multitype_container.h
@@ -0,0 +1,58 @@
+#pragma once
+
+namespace utils
+	{
+	template <template<class> class Container_type, typename ...Types>
+	class multitype_container
+		{
+		public:
+			template <typename ...Args>
+			multitype_container(Args&&... args) : containers { std::forward<Args>(args)... } {}
+
+			using tuple_t = std::tuple<Container_type<Types>...>;
+
+			tuple_t containers;
+
+			template <typename Type>
+			auto& get_containing_type()
+				{
+				return std::get<get_index_containing_type<Type>()>(containers);
+				}
+
+			template <typename Function>
+			void for_each_container(Function function) { utils::tuple::for_each_in_tuple(containers, [&](auto& container) { function(container); }); }
+			template <typename Function>
+			void for_each_element(Function function) { for_each_container([&](auto& container) { for (auto& element : container) { function(element); } }); }
+
+			template <typename Type, typename Function>
+			void for_each_element_of_type(Function function)
+				{
+				for_each_containing_type<Type>([&](auto& container) { for (auto& element : container) { function(element); } });
+				}
+
+			template <typename Type, typename Function>
+			void for_each_containing_type(Function function)
+				{
+				utils::tuple::for_each_in_tuple(containers, [&](auto& container)
+					{
+					if constexpr (std::is_base_of_v<Type, typename std::remove_reference_t<decltype(container)>::value_type>) { function(container); }
+					});
+				}
+
+		private:
+
+			template<typename T, std::size_t index = 0>
+			static constexpr std::size_t get_index_containing_type()
+				{
+				if constexpr (std::is_same_v<T, typename std::tuple_element<index, tuple_t>::type::value_type>) { return index; }
+				else { return get_index_containing_type<T, index + 1>(); }
+				}
+			template<typename T, std::size_t index = 0>
+			static constexpr std::size_t get_index_containing_type_or_derived()
+				{
+				if constexpr (std::is_base_of<T, typename std::tuple_element<index, tuple_t>::type::value_type>) { return index; }
+				else { return get_index_containing_type_or_derived<T, index + 1>(); }
+				}
+		};
+
+	}
---
+++ include/utils/containers/polymorphic_container.h
@@ -0,0 +1,97 @@
+#pragma once
+#include "multitype_container.h"
+#include "polyval.h"
+
+namespace utils
+	{
+	template<template<typename> typename Container_type, typename T>
+	struct container_emplace_helper
+		{};
+
+	template<typename T>
+	struct container_emplace_helper<std::vector, T>
+		{
+		template<typename... Args>
+		static T& emplace(std::vector<T>& v, Args&&... args)
+			{
+			return v.emplace_back(std::forward<Args>(args)...);
+			}
+		};
+
+	//TODO ensure that all Types inherit from Base_type
+	template <template<class> class Container_type, typename Base_type, typename ...Types>
+	class polymorphic_container
+		{
+		public:
+			polymorphic_container() = default;
+
+			template <typename Arg_parent, typename ...Args> //TODO can I split the variadic?
+			polymorphic_container(Arg_parent&& arg, Args&& ...args)
+				: parent_container{std::forward<Arg_parent>(arg)}, set{std::forward<Args>(args)...} {}
+
+			template <typename Type, typename ...Args>
+			Type& emplace(Args ...args)
+				{
+				if constexpr (utils::variadic::contains_type<Type, Types...>::value)
+					{
+					return container_emplace_helper<Container_type, Type>::emplace(set.get_containing_type<Type>(), std::forward<Args>(args)...);
+
+					//return set.get_containing_type<Type>().emplace_back(std::forward<Args>(args)...);
+					}
+				else 
+					{ 
+					return
+						static_cast<Type&>(
+							*container_emplace_helper<Container_type, utils::polymorphic_value<Base_type>>
+							::emplace(
+								parent_container, 
+								utils::make_polymorphic_value<Type>(std::forward<Args>(args)...)));
+					
+					
+					//return static_cast<Type&>(
+					//	*parent_container.emplace_back(std::move(utils::make_polymorphic_value<Type>(std::forward<Args>(args)...)))); 
+					}
+				}
+
+			template <typename Function>
+			void for_each_container(Function function) { function(parent_container); set.for_each_container(function); }
+			template <typename Function>
+			void for_each_element(Function function)
+				{
+				for (utils::polymorphic_value<Base_type>& polyval : parent_container) { function(*polyval); }
+				set.for_each_element(function);
+				}
+
+			template <typename Type, typename Function>
+			void for_each_containing_type(Function function) { set.for_each_containing_type<Type>(function); }
+
+			template <typename Type, typename Function>
+			void for_each_element_of_type(Function function)
+				{
+				for (utils::polymorphic_value<Base_type>& element : parent_container)
+					{
+					Type* element_cast = dynamic_cast<Type*>(&*element);
+					if (element_cast != nullptr)
+						{
+						function(*element_cast);
+						}
+					}
+
+				set.for_each_element_of_type<Type>(function);
+				}
+
+			template <typename Type>
+			auto& get()
+				{
+				if constexpr (utils::variadic::contains_type<Type, Types...>::value) { return set.get_containing_type<Type>(); }
+				else { return parent_container; }
+				}
+
+		private:
+
+			Container_type<utils::polymorphic_value<Base_type>> parent_container;
+			utils::multitype_container<Container_type, Types...> set;
+
+		};
+
+	}
