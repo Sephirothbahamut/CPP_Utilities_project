0c1e88891022032e4b64b3fa0e99df563272ca77
sephirothbahamut <sephirothbahamut@live.it>
2021-09-29 00:57:29 +0200

    made join and matrix_dyn more standard compliant

--- include/utils/algorithm/join.h
+++ include/utils/algorithm/join.h
@@ -5,11 +5,12 @@
 
 namespace utils::algorithm
 	{
-	template <typename T>
-	std::string join(typename T::const_iterator begin, typename T::const_iterator end, const std::string& separator)
+	template <typename input_iteratorr>
+	std::string join(input_iteratorr begin, input_iteratorr end, const std::string& separator)
 		{
 		if (begin == end) { return ""; }
-		std::stringstream ss{*begin};
+		std::stringstream ss; 
+		ss << *begin;
 		begin++;
 		for (; begin != end; begin++) { ss << separator << *begin; }
 		return ss.str();
--- include/utils/containers/matrix.h
+++ include/utils/containers/matrix.h
@@ -3,48 +3,60 @@
 #include <vector>
 #include <stdexcept>
 
-
 namespace utils
 	{
 	enum matrix_memory { width_first, height_first };
 
-	template <typename T, matrix_memory MEMORY = matrix_memory::width_first >
+	template <typename T, matrix_memory MEMORY = matrix_memory::width_first, class Allocator = std::allocator<T>>
 	class matrix_dyn
 		{
+		using inner_container_t = std::vector<T, Allocator>;
+
 		public:
-			matrix_dyn(size_t width, size_t height) : _width(width), _height(height), _arr(width * height) {}
+			using value_type             = inner_container_t::value_type;
+			using size_type              = inner_container_t::size_type;
+			using reference              = inner_container_t::reference;
+			using const_reference        = inner_container_t::const_reference;
+			using pointer                = inner_container_t::pointer;
+			using const_pointer          = inner_container_t::const_pointer;
+			using iterator               = inner_container_t::iterator;
+			using const_iterator         = inner_container_t::const_iterator;
+			using reverse_iterator       = inner_container_t::reverse_iterator;
+			using const_reverse_iterator = inner_container_t::const_reverse_iterator;
 
-			using coords_t = std::pair<size_t, size_t>;
+			struct coords_t { size_type x; size_type y; };
 
-			const size_t width()  const noexcept { return _width; }
-			const size_t height() const noexcept { return _height; }
-			const size_t size()   const noexcept { return _width * _height; }
+			matrix_dyn(size_type width, size_type height) : _width(width), _height(height), _arr(width* height) {}
 
-			const T* data() const noexcept { return _arr.data(); }
-			      T* data()       noexcept { return _arr.data(); }
+			const size_type width()  const noexcept { return _width; }
+			const size_type height() const noexcept { return _height; }
+			const size_type size()   const noexcept { return _width * _height; }
 
-			const std::vector<T>& get_vector() const noexcept { return _arr; }
-			      std::vector<T>& get_vector()       noexcept { return _arr; }
+			const_pointer data() const noexcept { return _arr.data(); }
+			      pointer data()       noexcept { return _arr.data(); }
 
-			size_t get_index(size_t x, size_t y) const noexcept
+			const inner_container_t& get_vector() const noexcept { return _arr; }
+			      inner_container_t& get_vector()       noexcept { return _arr; }
+
+			size_type get_index(size_type x, size_type y) const noexcept
 				{
 				if constexpr (MEMORY == matrix_memory::width_first) { return x + (y * _width); }
 				else { return y + (x * _height); }
 				}
-			size_t get_x(size_t index) const noexcept { if constexpr (MEMORY == matrix_memory::width_first) { return index % width(); } else { return index / height(); } } //TODO test
-			size_t get_y(size_t index) const noexcept { if constexpr (MEMORY == matrix_memory::width_first) { return index / width(); } else { return index % height(); } } //TODO test
-			coords_t get_coords(size_t index) const noexcept { return {get_x(index), get_y(index)}; }
+			size_type get_x     (size_type index) const noexcept { if constexpr (MEMORY == matrix_memory::width_first) { return index % width(); } else { return index / height(); } } //TODO test
+			size_type get_y     (size_type index) const noexcept { if constexpr (MEMORY == matrix_memory::width_first) { return index / width(); } else { return index % height(); } } //TODO test
+			coords_t  get_coords(size_type index) const noexcept { return {get_x(index), get_y(index)}; }
 			
-			const T& operator[](size_t i)        const noexcept { return _arr[i]; }
-			      T& operator[](size_t i)              noexcept { return _arr[i]; }
-			const T& operator[](coords_t coords) const noexcept { return _arr[get_index(coords.first, coords.second)]; }
-			      T& operator[](coords_t coords)       noexcept { return _arr[get_index(coords.first, coords.second)]; }
-			const T& at(size_t i)                const          { if (i >= size())                 { throw std::out_of_range{"Matrix access out of bounds."}; } return operator[](i); }
-			      T& at(size_t i)                               { if (i >= size())                 { throw std::out_of_range{"Matrix access out of bounds."}; } return operator[](i); }
-			const T& at(size_t x, size_t y)      const          { if (x >= _width || y >= _height) { throw std::out_of_range{"Matrix access out of bounds."}; } return operator[]({x, y}); }
-			      T& at(size_t x, size_t y)                     { if (x >= _width || y >= _height) { throw std::out_of_range{"Matrix access out of bounds."}; } return operator[]({x, y}); }
-			const T& at(coords_t coords)         const          { return at(coords.first, coords.second); }
-			      T& at(coords_t coords)                        { return at(coords.first, coords.second); }
+			const_reference operator[](size_type i)      const noexcept { return _arr[i]; }
+			      reference operator[](size_type i)            noexcept { return _arr[i]; }
+			const_reference operator[](coords_t  coords) const noexcept { return _arr[get_index(coords.x, coords.y)]; }
+			      reference operator[](coords_t  coords)       noexcept { return _arr[get_index(coords.x, coords.y)]; }
+			const_reference at(size_type i)              const          { if (i >= size())                 { throw std::out_of_range{"Matrix access out of bounds."}; } return operator[](i); }
+			      reference at(size_type i)                             { if (i >= size())                 { throw std::out_of_range{"Matrix access out of bounds."}; } return operator[](i); }
+			const_reference at(size_type x, size_type y) const          { if (x >= _width || y >= _height) { throw std::out_of_range{"Matrix access out of bounds."}; } return operator[]({x, y}); }
+			      reference at(size_type x, size_type y)                { if (x >= _width || y >= _height) { throw std::out_of_range{"Matrix access out of bounds."}; } return operator[]({x, y}); }
+			const_reference at(coords_t coords)          const          { return at(coords.x, coords.y); }
+			      reference at(coords_t coords)                         { return at(coords.x, coords.y); }
 
 			// Arithmetic
 
@@ -73,9 +85,9 @@ namespace utils
 			friend std::ostream& cout::operator<<(std::ostream& os, const matrix_dyn<T, MEMORY>& la);
 #endif
 		private:
-			size_t _width;
-			size_t _height;
-			std::vector<T> _arr;
+			size_type _width;
+			size_type _height;
+			std::vector<T, Allocator> _arr;
 
 #ifdef UTILS_COUT
 			std::ostream& out(std::ostream& os) const
@@ -85,7 +97,7 @@ namespace utils
 				ccu::brace(os, "[");
 				if constexpr (MEMORY == matrix_memory::width_first)
 					{
-					for (size_t i = 0; i < size() - 1; i++)
+					for (size_type i = 0; i < size() - 1; i++)
 						{
 						ccu::value(os, (*this)[i]);
 						ccu::separ(os, ", ");
@@ -94,16 +106,16 @@ namespace utils
 					}
 				else
 					{
-					for (size_t y = 0; y < height() - 1; y++)
+					for (size_type y = 0; y < height() - 1; y++)
 						{
-						for (size_t x = 0; x < width(); x++)
+						for (size_type x = 0; x < width(); x++)
 							{
 							ccu::value(os, (*this)[get_index(x, y)]);
 							ccu::separ(os, ", ");
 							}
 						os << "\n ";
 						}
-					for (size_t x = 0; x < width() - 1; x++)
+					for (size_type x = 0; x < width() - 1; x++)
 						{
 						ccu::value(os, (*this)[get_index(x, height() - 1)]);
 						ccu::separ(os, ", ");
