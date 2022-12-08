40c6d150aabb2cd3038714327ecb998246052249
sephirothbahamut <sephirothbahamut@live.it>
2022-12-08 06:07:32 +0100

    added comment

--- include/utils/virtual.h
+++ include/utils/virtual.h
@@ -4,6 +4,9 @@
 //#define utils_devirtualize protected: virtual void utils_i_am_virtual() final override { };
 namespace utils
 	{
+	/// <summary>
+	/// This class should be inherited virtually.
+	/// </summary>
 	struct virtualize { virtual void utils_i_am_virtual() = 0; };
 	struct devirtualize : virtual virtualize { virtual void utils_i_am_virtual() final override {} };
 	}
