d7dcf40eb15db07e9e371dfb8f45ae4938c8449f
sephirothbahamut <sephirothbahamut@live.it>
2022-12-10 21:55:28 +0100

    started templatifying geometry

--- include/utils/math/geometry/aabb.h
+++ include/utils/math/geometry/aabb.h
@@ -1,94 +1,121 @@
 #pragma once
 
+#include "../../console/colour.h"
 #include "../vec2.h"
 
 namespace utils::math::geometry
 	{
-	struct axis_aligned_bounding_box
+	template <typename T = float>
+	struct aabb
 		{
 #pragma region Variables
-		float ll;
-		float up;
-		float rr;
-		float dw;
+		T rr;
+		T up;
+		T ll;
+		T dw;
 #pragma endregion Variables
 
 #pragma region Accessors
 		// Corners
-		float get_up()  const noexcept { return up; }
-		float get_dw()  const noexcept { return dw; }
-		float get_rr()  const noexcept { return rr; }
-		float get_ll()  const noexcept { return ll; }
-		void  set_up(float v) noexcept { up = v; }
-		void  set_dw(float v) noexcept { dw = v; }
-		void  set_rr(float v) noexcept { rr = v; }
-		void  set_ll(float v) noexcept { ll = v; }
+		T    get_up() const noexcept { return up; }
+		T    get_dw() const noexcept { return dw; }
+		T    get_rr() const noexcept { return rr; }
+		T    get_ll() const noexcept { return ll; }
+		void set_up(T v)    noexcept { up = v; }
+		void set_dw(T v)    noexcept { dw = v; }
+		void set_rr(T v)    noexcept { rr = v; }
+		void set_ll(T v)    noexcept { ll = v; }
 
-		vec2f get_ul()      const noexcept { return {ll, up}; }
-		vec2f get_ur()      const noexcept { return {rr, up}; }
-		vec2f get_dr()      const noexcept { return {rr, dw}; }
-		vec2f get_dl()      const noexcept { return {ll, dw}; }
-		void  set_ul(vec2f value) noexcept { ll = value.x; up = value.y; }
-		void  set_ur(vec2f value) noexcept { rr = value.x; up = value.y; }
-		void  set_dr(vec2f value) noexcept { rr = value.x; dw = value.y; }
-		void  set_dl(vec2f value) noexcept { ll = value.x; dw = value.y; }
+		vec2<T> get_ul()        const noexcept { return {ll, up}; }
+		vec2<T> get_ur()        const noexcept { return {rr, up}; }
+		vec2<T> get_dr()        const noexcept { return {rr, dw}; }
+		vec2<T> get_dl()        const noexcept { return {ll, dw}; }
+		void    set_ul(vec2<T> value) noexcept { ll = value.x; up = value.y; }
+		void    set_ur(vec2<T> value) noexcept { rr = value.x; up = value.y; }
+		void    set_dr(vec2<T> value) noexcept { rr = value.x; dw = value.y; }
+		void    set_dl(vec2<T> value) noexcept { ll = value.x; dw = value.y; }
 
 		// Pos-size
-		float get_x()      const noexcept { return ll; }
-		float get_y()      const noexcept { return up; }
-		float get_h()      const noexcept { return dw - up; }
-		float get_w()      const noexcept { return rr - ll; }
-		vec2f get_s()      const noexcept { return {w, h}; }
-		vec2f get_p()      const noexcept { return ul; }
-		void  set_x(float value) noexcept { float tmp = w; ll = value; w = tmp; }
-		void  set_y(float value) noexcept { float tmp = h; up = value; h = tmp; }
-		void  set_h(float value) noexcept { dw = up + value; }
-		void  set_w(float value) noexcept { rr = ll + value; }
-		void  set_s(vec2f value) noexcept { w = value.x; h = value.y; }
-		void  set_p(vec2f value) noexcept { x = value.x; y = value.y; }
+		T       get_x()        const noexcept { return ll; }
+		T       get_y()        const noexcept { return up; }
+		T       get_h()        const noexcept { return dw - up; }
+		T       get_w()        const noexcept { return rr - ll; }
+		vec2<T> get_s()        const noexcept { return {w, h}; }
+		vec2<T> get_p()        const noexcept { return ul; }
+		void    set_x(T       value) noexcept { T tmp = w; ll = value; w = tmp; }
+		void    set_y(T       value) noexcept { T tmp = h; up = value; h = tmp; }
+		void    set_h(T       value) noexcept { dw = up + value; }
+		void    set_w(T       value) noexcept { rr = ll + value; }
+		void    set_s(vec2<T> value) noexcept { w = value.x; h = value.y; }
+		void    set_p(vec2<T> value) noexcept { x = value.x; y = value.y; }
 #pragma endregion Accessors
 
 #pragma region Properties
 		// Aliases
-		__declspec(property(get = get_up, put = set_up)) float top;
-		__declspec(property(get = get_dw, put = set_dw)) float down;
-		__declspec(property(get = get_dw, put = set_dw)) float bottom;
-		__declspec(property(get = get_ll, put = set_ll)) float left;
-		__declspec(property(get = get_rr, put = set_rr)) float right;
-		__declspec(property(get = get_ll, put = set_ll)) float x1;
-		__declspec(property(get = get_rr, put = set_rr)) float x2;
-		__declspec(property(get = get_up, put = set_up)) float y1;
-		__declspec(property(get = get_dw, put = set_dw)) float y2;
+		__declspec(property(get = get_up, put = set_up)) T top;
+		__declspec(property(get = get_dw, put = set_dw)) T down;
+		__declspec(property(get = get_dw, put = set_dw)) T bottom;
+		__declspec(property(get = get_ll, put = set_ll)) T left;
+		__declspec(property(get = get_rr, put = set_rr)) T right;
+		__declspec(property(get = get_ll, put = set_ll)) T x1;
+		__declspec(property(get = get_rr, put = set_rr)) T x2;
+		__declspec(property(get = get_up, put = set_up)) T y1;
+		__declspec(property(get = get_dw, put = set_dw)) T y2;
 
 		// Corners
-		__declspec(property(get = get_ul, put = set_ul)) vec2f ul;
-		__declspec(property(get = get_ul, put = set_ul)) vec2f up_left;
-		__declspec(property(get = get_ul, put = set_ul)) vec2f top_left;
-		__declspec(property(get = get_ur, put = set_ur)) vec2f ur;
-		__declspec(property(get = get_ur, put = set_ur)) vec2f up_right;
-		__declspec(property(get = get_ur, put = set_ur)) vec2f top_right;
-		__declspec(property(get = get_dr, put = set_dr)) vec2f dr;
-		__declspec(property(get = get_dr, put = set_dr)) vec2f down_right;
-		__declspec(property(get = get_dr, put = set_dr)) vec2f bottom_right;
-		__declspec(property(get = get_dl, put = set_dl)) vec2f dl;
-		__declspec(property(get = get_dl, put = set_dl)) vec2f down_left;
-		__declspec(property(get = get_dl, put = set_dl)) vec2f bottom_left;
+		__declspec(property(get = get_ul, put = set_ul)) vec2<T> ul;
+		__declspec(property(get = get_ul, put = set_ul)) vec2<T> up_left;
+		__declspec(property(get = get_ul, put = set_ul)) vec2<T> top_left;
+		__declspec(property(get = get_ur, put = set_ur)) vec2<T> ur;
+		__declspec(property(get = get_ur, put = set_ur)) vec2<T> up_right;
+		__declspec(property(get = get_ur, put = set_ur)) vec2<T> top_right;
+		__declspec(property(get = get_dr, put = set_dr)) vec2<T> dr;
+		__declspec(property(get = get_dr, put = set_dr)) vec2<T> down_right;
+		__declspec(property(get = get_dr, put = set_dr)) vec2<T> bottom_right;
+		__declspec(property(get = get_dl, put = set_dl)) vec2<T> dl;
+		__declspec(property(get = get_dl, put = set_dl)) vec2<T> down_left;
+		__declspec(property(get = get_dl, put = set_dl)) vec2<T> bottom_left;
 
 		// Pos-size
-		__declspec(property(get = get_x, put = set_x)) float x;
-		__declspec(property(get = get_y, put = set_y)) float y;
-		__declspec(property(get = get_h, put = set_h)) float h;
-		__declspec(property(get = get_h, put = set_h)) float height;
-		__declspec(property(get = get_w, put = set_w)) float w;
-		__declspec(property(get = get_w, put = set_w)) float width;
-		__declspec(property(get = get_s, put = set_s)) vec2f s;
-		__declspec(property(get = get_s, put = set_s)) vec2f size;
-		__declspec(property(get = get_p, put = set_p)) vec2f p;
-		__declspec(property(get = get_p, put = set_p)) vec2f pos;
-		__declspec(property(get = get_p, put = set_p)) vec2f position;
+		__declspec(property(get = get_x, put = set_x)) T x;
+		__declspec(property(get = get_y, put = set_y)) T y;
+		__declspec(property(get = get_h, put = set_h)) T h;
+		__declspec(property(get = get_h, put = set_h)) T height;
+		__declspec(property(get = get_w, put = set_w)) T w;
+		__declspec(property(get = get_w, put = set_w)) T width;
+		__declspec(property(get = get_s, put = set_s)) vec2<T> s;
+		__declspec(property(get = get_s, put = set_s)) vec2<T> size;
+		__declspec(property(get = get_p, put = set_p)) vec2<T> p;
+		__declspec(property(get = get_p, put = set_p)) vec2<T> pos;
+		__declspec(property(get = get_p, put = set_p)) vec2<T> position;
 #pragma endregion Properties
 
-		bool contains(vec2f point) const noexcept { return point.x >= ll && point.x <= rr && point.y >= up && point.y <= dw; }
+		bool contains(vec2<T> point) const noexcept { return point.x >= ll && point.x <= rr && point.y >= up && point.y <= dw; }
 		};
-	using aabb = axis_aligned_bounding_box;
+
+	template <typename T = float>
+	using axis_aligned_bounding_box = aabb<T>;
+	}
+
+
+
+namespace utils::output
+	{
+	namespace typeless
+		{
+		template <typename T>
+		inline ::std::ostream& operator<<(::std::ostream& os, const utils::math::geometry::aabb<T>& aabb)
+			{
+			os << "(left: " << aabb.ll << ", up: " << aabb.up << ", right: " << aabb.rr << ", down: " << aabb.dw << ")";
+			return os;
+			}
+		}
+
+	template <typename T>
+	inline ::std::ostream& operator<<(::std::ostream& os, const utils::math::geometry::aabb<T>& aabb)
+		{
+		namespace ucc = utils::console::colour;
+		os << ucc::type << "aabb<" << typeid(T).name() << ">";
+		return utils::output::typeless::operator<<(os, aabb);
+		}
 	}
