c67f23859b84f1ed71deb766508522311e66a2e3
sephirothbahamut <sephirothbahamut@live.it>
2023-10-10 20:56:18 +0200

    added todo devices

---
+++ include/utils/input_system/device/gamepad.h
@@ -0,0 +1,29 @@
+#pragma once
+
+
+//enum class button_id
+//	{
+//	dpad_right,
+//	dpad_up,
+//	dpad_left,
+//	dpad_down,
+//	a,
+//	b,
+//	x,
+//	y,
+//	shoulder_r1,
+//	shoulder_l1,
+//	shoulder_r2,
+//	shoulder_l2,
+//	thumbstick_right,
+//	thumbstick_left,
+//	start,
+//	select
+//	};
+//enum class axis_id
+//	{
+//	thumbstick_right_x,
+//	thumbstick_right_y,
+//	thumbstick_left_x,
+//	thumbstick_left_y
+//	};
