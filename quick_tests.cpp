659349a1a91900b582d6a651aa3f5c52e7c3446a
sephirothbahamut <sephirothbahamut@live.it>
2024-07-07 21:01:31 +0200

    added missing files to project

--- quick_tests.cpp
+++ quick_tests.cpp
@@ -2,58 +2,58 @@
 #include <iostream>
 #include <execution>
 
-#include "include/utils/memory.h"
+#include "Library/include/utils/memory.h"
 
-#include "include/utils/console/io.h"
-#include "include/utils/console/colour.h"
-#include "include/utils/console/initializer.h"
+#include "Library/include/utils/console/io.h"
+#include "Library/include/utils/console/colour.h"
+#include "Library/include/utils/console/initializer.h"
 
-#include "include/utils/math/vec.h"
-#include "include/utils/math/vec2.h"
-#include "include/utils/math/vec3.h"
-#include "include/utils/math/vec_s.h"
-#include "include/utils/math/angle.h"
-#include "include/utils/math/rect.h"
+#include "Library/include/utils/math/vec.h"
+#include "Library/include/utils/math/vec2.h"
+#include "Library/include/utils/math/vec3.h"
+#include "Library/include/utils/math/vec_s.h"
+#include "Library/include/utils/math/angle.h"
+#include "Library/include/utils/math/rect.h"
 
-#include "include/utils/output/std_containers.h"
-#include "include/utils/graphics/colour.h"
+#include "Library/include/utils/output/std_containers.h"
+#include "Library/include/utils/graphics/colour.h"
 
-#include "include/utils/logging/logger.h"
+#include "Library/include/utils/logging/logger.h"
 
-#include "include/utils/clock.h"
-//#include "include/utils/containers/handled_container.h"
-//#include "include/utils/containers/multihandled.h"
+#include "Library/include/utils/clock.h"
+//#include "Library/include/utils/containers/handled_container.h"
+//#include "Library/include/utils/containers/multihandled.h"
 
-//#include "include/utils/containers/hive/next.h"
-//#include "include/utils/containers/linked_vector.h"
-//#include "include/utils/containers/segmented_vector.h"
+//#include "Library/include/utils/containers/hive/next.h"
+//#include "Library/include/utils/containers/linked_vector.h"
+//#include "Library/include/utils/containers/segmented_vector.h"
 //
-//#include "include/utils/containers/multithreading/multiqueue_consumer.h"
+//#include "Library/include/utils/containers/multithreading/multiqueue_consumer.h"
 //
-#include "include/utils/containers/object_pool.h"
-#include "include/utils/matrix_interface.h"
+#include "Library/include/utils/containers/object_pool.h"
+#include "Library/include/utils/matrix_interface.h"
 //
-//#include "include/utils/oop/counting.h"
+//#include "Library/include/utils/oop/counting.h"
 //
-//#include "include/utils/thread_pool.h"
+//#include "Library/include/utils/thread_pool.h"
 //
-#include "include/utils/beta/containers/multihandled_default.h"
+#include "Library/include/utils/beta/containers/multihandled_default.h"
 //
-#include "include/utils/name.h"
+#include "Library/include/utils/name.h"
 //
-#include "include/utils/tracking.h"
-//#include "include/utils/containers/resource_manager.h"
+#include "Library/include/utils/tracking.h"
+//#include "Library/include/utils/containers/resource_manager.h"
 
-#include "include/utils/storage.h"
+#include "Library/include/utils/storage.h"
 
-//#include "include/utils/beta/math/geometry/all.h"
-//#include "include/utils/beta/math/geometry/group.h"
-//#include "include/utils/math/transform2.h"
-#include "include/utils/math/geometry/shapes.h"
+//#include "Library/include/utils/beta/math/geometry/all.h"
+//#include "Library/include/utils/beta/math/geometry/group.h"
+//#include "Library/include/utils/math/transform2.h"
+#include "Library/include/utils/math/geometry/shapes.h"
 
-#include "include/utils/math/geometry/interactions.h"
+#include "Library/include/utils/math/geometry/interactions.h"
 
-#include "include/utils/output/vec.h"
+#include "Library/include/utils/output/vec.h"
 
 //using civ = utils::oop::counting_invalidating_move;
 //
@@ -63,8 +63,8 @@
 //	int v;
 //	};
 
-#include "include/utils/third_party/stb_image.h"
-#include "include/utils/third_party/stb_image_write.h"
+#include "Library/include/utils/third_party/stb_image.h"
+#include "Library/include/utils/third_party/stb_image_write.h"
 
 
 
@@ -78,11 +78,7 @@ int main()
 	using namespace utils::output;
 	using namespace utils::math::angle::literals;
 
-	
 
-	
-	
-	
 
 	return 0;
 	}
