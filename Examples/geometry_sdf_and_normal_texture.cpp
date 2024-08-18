4cf9dc1b556ff78fc2028d6bf975559173e65ddf
sephirothbahamut <sephirothbahamut@live.it>
2024-08-18 03:58:13 +0200

    updated libraryy

--- Examples/geometry_sdf_and_normal_texture.cpp
+++ Examples/geometry_sdf_and_normal_texture.cpp
@@ -2,6 +2,7 @@
 #include <ranges>
 #include <execution>
 
+#include <utils/math/vec2.h>
 #include <utils/math/vec3.h>
 #include <utils/math/vec_s.h>
 #include <utils/math/geometry/all.h>
@@ -9,7 +10,6 @@
 #include <utils/third_party/stb_image_write.h>
 
 #include <utils/clock.h>
-
 float smoothstep(float edge0, float edge1, float x) 
 	{
 	// Scale, bias and saturate x to 0..1 range
@@ -43,32 +43,55 @@ void geometry_sdf_and_normal_texture()
 		utils::math::vec2f{100.f,   0.f},
 		utils::math::vec2f{ 50.f,  50.f}
 		};
-
-	utils::math::geometry::shape::mixed mixed{utils::math::vec2f{600.f, 300.f}};
-	mixed.add_segments
-		({
-		utils::math::vec2f{620.f, 290.f},
-		utils::math::vec2f{640.f, 320.f},
-		utils::math::vec2f{640.f, 350.f}
-		});
+	
+	//utils::math::geometry::shape::mixed<utils::math::geometry::ends::create::open()> mixed{utils::math::vec2f{130.f, 630.f}};
+	//mixed.add_segments
+	//	({
+	//	utils::math::vec2f{180.f, 580.f},
+	//	utils::math::vec2f{240.f, 600.f}
+	//	});
+	//mixed.add_bezier
+	//	({
+	//	utils::math::vec2f{180.f, 640.f},
+	//	utils::math::vec2f{230.f, 730.f}
+	//	});
+	//mixed.add_segment({150.f, 690.f});
+	utils::math::geometry::shape::mixed<utils::math::geometry::ends::closeable::create::closed()> mixed{utils::math::vec2f{120.f, 580.f}};
+	mixed.add_segment({240.f, 600.f});
 	mixed.add_bezier
 		({
-		utils::math::vec2f{640.f, 380.f},
-		utils::math::vec2f{580.f, 360.f}
+		utils::math::vec2f{180.f, 640.f},
+		utils::math::vec2f{230.f, 730.f}
 		});
 
-	utils::math::geometry::shape::observer::segment obseg{vertices[0], vertices[1]};
-	utils::math::geometry::interactions::translate(obseg, utils::math::vec2f{1.f, 2.3333f});
-	utils::math::geometry::interactions::side(obseg, vertices[3]);
-
+	utils::math::geometry::shape::mixed mixed_inverse{utils::math::vec2f{150.f, 635.f}};
+	mixed_inverse.add_segments
+		({
+		utils::math::vec2f{166.f, 684.f},
+		utils::math::vec2f{197.f, 700.f}
+		});
+	mixed_inverse.add_bezier
+		({
+		utils::math::vec2f{160.f, 640.f},
+		utils::math::vec2f{214.f, 606.f}
+		});
+	mixed_inverse.add_segment({183.f, 592.f});
 
 	// Open polyline defined on a statically sized span of vertices over a sequence in memory, with an infinite start and a finite end
-	utils::math::geometry::shape::observer::polyline<utils::math::geometry::ends::create::open(true, false), 5> poyline{.vertices{vertices.begin(), size_t{5}}};
+	utils::math::geometry::shape::observer::polyline<utils::math::geometry::ends::closeable::create::open(true, false), 5> poyline{.vertices{vertices.begin(), size_t{5}}};
 
 	// Closed polygon defined on a statically sized span of vertices over a sequence in memory
 	utils::math::geometry::shape::observer::polygon<> triangle_b{.vertices{vertices.begin() + 5, size_t{3}}};
 
-	utils::math::geometry::shape::bezier<3> bezier{.vertices{utils::math::vec2f{300.f, 300.f}, utils::math::vec2f{200.f, 600.f}, utils::math::vec2f{800.f, 400.f}}};
+	using bezier_t = utils::math::geometry::shape::bezier
+		<
+		3,
+		utils::math::geometry::ends::optional_ab::create::value
+			(
+			utils::math::geometry::ends::ab::create::default_(false, true)
+			)
+		>;
+	bezier_t bezier{.vertices{utils::math::vec2f{800.f, 400.f}, utils::math::vec2f{200.f, 600.f}, utils::math::vec2f{300.f, 300.f}}};
 
 	utils::math::geometry::interactions::translate_self(poyline   , {50.f, 50.f});
 	utils::math::geometry::interactions::scale_self    (triangle  , .5f);
@@ -81,7 +104,11 @@ void geometry_sdf_and_normal_texture()
 	utils::storage::multiple<utils::graphics::colour::rgba_u> image{image_sizes.sizes_to_size()};
 	std::ranges::iota_view indices(size_t{0}, image.size());
 
-	std::for_each(/*std::execution::par,*/ indices.begin(), indices.end(), [&](size_t index)
+	//*
+	std::for_each(std::execution::par, indices.begin(), indices.end(), [&](size_t index)
+	/*/
+	std::for_each(indices.begin(), indices.end(), [&](size_t index)
+	/**/
 		{
 		const utils::math::vec2s coords_indices{image_sizes.index_to_coords(index)};
 		const utils::math::vec2f coords_f
@@ -90,24 +117,27 @@ void geometry_sdf_and_normal_texture()
 			static_cast<float>(coords_indices.y())
 			};
 
+		if (coords_indices == utils::math::vec2s{size_t{68}, size_t{155}})
+			{
+			std::cout << "a";
+			}
+
+		const auto cwsd_mixed_outer  {utils::math::geometry::interactions::closest_with_signed_distance(mixed        , coords_f)};
+		//const auto cwsd_mixed_inverse{utils::math::geometry::interactions::closest_with_signed_distance(mixed_inverse, coords_f)};
+		//const auto cwsd_mixed{utils::math::geometry::interactions::return_types::closest_point_with_signed_distance::pick_closest(cwsd_mixed_outer, cwsd_mixed_inverse)};
+
 		std::array gdists
 			{
-			utils::math::geometry::interactions::gradient_signed_distance(poyline   , coords_f),
-			utils::math::geometry::interactions::gradient_signed_distance(triangle  , coords_f),
-			utils::math::geometry::interactions::gradient_signed_distance(triangle_b, coords_f),
-			utils::math::geometry::interactions::gradient_signed_distance(bezier    , coords_f),
+			//utils::math::geometry::interactions::gradient_signed_distance(poyline   , coords_f),
+			//utils::math::geometry::interactions::gradient_signed_distance(triangle  , coords_f),
+			//utils::math::geometry::interactions::gradient_signed_distance(triangle_b, coords_f),
+			//utils::math::geometry::interactions::gradient_signed_distance(bezier    , coords_f),
+			utils::math::geometry::interactions::return_types::gradient_signed_distance
+				{
+				.distance{cwsd_mixed_outer.distance},
+				.gradient{1.f, 0.f}
+				},
 			};
-
-		std::array mdists
-			{
-			utils::math::geometry::interactions::minimum_distance(mixed     , coords_f),
-			utils::math::geometry::interactions::minimum_distance(poyline   , coords_f),
-			utils::math::geometry::interactions::minimum_distance(triangle  , coords_f),
-			utils::math::geometry::interactions::minimum_distance(triangle_b, coords_f),
-			utils::math::geometry::interactions::minimum_distance(bezier    , coords_f),
-			};
-		gdists[0].distance.value = gdists[0].distance.absolute();
-		gdists[3].distance.value = gdists[3].distance.absolute();
 
 		utils::math::geometry::interactions::return_types::gradient_signed_distance gdist;
 		for (const auto& tmp : gdists)
@@ -115,11 +145,6 @@ void geometry_sdf_and_normal_texture()
 			gdist = utils::math::geometry::interactions::return_types::gradient_signed_distance::merge(gdist, tmp);
 			}
 		
-		float mdist{utils::math::constants::finf};
-		for (const auto& tmp : mdists)
-			{
-			mdist = utils::math::min(mdist, tmp);
-			}
 
 		gdist.distance.value *= .006f;
 
