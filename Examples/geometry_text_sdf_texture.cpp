b3ac581fa7c8bc4a688404b85dff07c969c000a6
sephirothbahamut <sephirothbahamut@live.it>
2024-08-20 19:36:38 +0200

    further testing

--- Examples/geometry_text_sdf_texture.cpp
+++ Examples/geometry_text_sdf_texture.cpp
@@ -16,6 +16,7 @@
 
 void geometry_text_sdf_texture()
 	{
+	// Testing outside of COM to make sure I'm not going crazy and my classes are fine
 	for (size_t i = 0; i < 200; i++)
 		{
 		std::vector<utils::graphics::text::glyph_t> vec;
@@ -25,12 +26,21 @@ void geometry_text_sdf_texture()
 			utils::graphics::text::glyph_t glyph{utils::math::vec2f{10.f, 10.f}};
 			glyph.add_segments({utils::math::vec2f{20.f, 20.f}, utils::math::vec2f{30.f, 30.f}, utils::math::vec2f{40.f, 40.f}});
 			glyph.add_bezier_3pt({utils::math::vec2f{50.f, 50.f}, utils::math::vec2f{60.f, 60.f}, utils::math::vec2f{70.f, 70.f}, utils::math::vec2f{80.f, 80.f}});
+
+			const utils::math::transform2 transform
+				{
+				.translation{1.f, 1.f}
+				};
+			utils::math::geometry::interactions::transform_self(glyph, transform);
+
+			if (glyph.vertices[0].x() != 11.f) { throw std::runtime_error{"Corrupted memory"}; }
+
 			vec.emplace_back(std::move(glyph));
 			}
 		}
 
 	//std::string string{(const char*)u8"Laelina\n\"£€$%&/()=?!^"};
-	std::string string{"I"};
+	std::string string{"Iqwerty"};
 	std::vector<utils::graphics::text::glyph_t> glyphs{utils::graphics::text::glyphs_from_string(string, L"Arial")};
 	std::vector<utils::math::geometry::shape::aabb> aabbs(glyphs.size());
 
@@ -39,7 +49,7 @@ void geometry_text_sdf_texture()
 		.translation{64.f, 64.f}
 		};
 
-	const float maximum_sdf_distance{ 32.f};
+	const float maximum_sdf_distance{32.f};
 
 	std::ranges::iota_view glyphs_indices(size_t{0}, glyphs.size());
 	std::for_each(std::execution::par, glyphs_indices.begin(), glyphs_indices.end(), [&](size_t index)
@@ -62,7 +72,7 @@ void geometry_text_sdf_texture()
 	utils::storage::multiple<utils::graphics::colour::rgba_u> image{image_sizes.sizes_to_size()};
 	std::ranges::iota_view indices(size_t{0}, image.size());
 
-	/*
+	//*
 	std::for_each(std::execution::par, indices.begin(), indices.end(), [&](size_t index)
 	/*/
 	std::for_each(indices.begin(), indices.end(), [&](size_t index)
@@ -86,7 +96,7 @@ void geometry_text_sdf_texture()
 		for (size_t glyphs_index{0}; glyphs_index < glyphs.size(); glyphs_index++)
 			{
 			const auto& aabb{aabbs[glyphs_index]};
-			//if (!aabb.contains(coords_f)) { continue; }
+			if (!aabb.contains(coords_f)) { continue; }
 
 			const auto& glyph{glyphs[glyphs_index]};
 
