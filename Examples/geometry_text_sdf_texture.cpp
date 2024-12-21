6e530391b6c4cd33bb066c204878a14801b75fc3
sephirothbahamut <sephirothbahamut@live.it>
2024-12-21 23:33:53 +0100

    added d2d geometry inside check to text rendering example

--- Examples/geometry_text_sdf_texture.cpp
+++ Examples/geometry_text_sdf_texture.cpp
@@ -55,6 +55,7 @@ void geometry_text_sdf_texture()
 	//std::string string{(const char*)u8"Freya"};
 	//std::string string{(const char*)u8"\n"};
 	std::string string{(const char*)u8"Hello, world!\nFreya\n\n"};
+	//std::string string{(const char*)u8"d"}; //"d" in MagicMedieval has self-intersecting curves that my code doesn't handle
 
 	utils::MS::graphics::text::formatted_string formatted_string{dx_initializer, string, text_format, output_resolution_f};
 
@@ -64,12 +65,14 @@ void geometry_text_sdf_texture()
 	formatted_string.properties_regions.rendering.text.colour.add({0.f, 1.f, 0.f, 1.f}, {15, 5});
 	formatted_string.properties_regions.rendering.text.colour.add({0.f, 1.f, 1.f, 1.f}, {20, 5});
 	formatted_string.properties_regions.rendering.text.colour.add({0.f, 0.f, 1.f, 1.f}, {25, 5});
+	//formatted_string.properties_regions.formatting.font.add("MagicMedieval", {0, 1});
 	//formatted_string.shrink_to_fit();
 	formatted_string.update();
 
 	text_renderer.draw_text(formatted_string, {0.f, 0.f});
 
 	const auto renderer_output{text_renderer.get_output()};
+	const auto renderer_dx_geometries{text_renderer.get_dx_geometry_output()};
 
 	std::filesystem::create_directories("./output");
 	utils::graphics::image::save_to_file(renderer_output.image, "output/text_directwrite.png");
@@ -117,6 +120,11 @@ void geometry_text_sdf_texture()
 			.scale    (1.f / supersampling)
 			};
 
+		if (coords_indices == utils::math::vec2s{size_t{522}, size_t{51}})
+			{
+			std::cout << "break";
+			}
+
 
 		utils::math::geometry::sdf::gradient_signed_distance gdist;
 		for (const auto& bb_outline : bb_outlines)
@@ -124,12 +132,12 @@ void geometry_text_sdf_texture()
 			const auto evaluated{bb_outline.evaluate_gradient_signed_distance(coords_f)};
 			gdist = utils::math::geometry::sdf::gradient_signed_distance::merge_absolute(gdist, evaluated);
 			}
-		
-		//utils::math::geometry::sdf::gradient_signed_distance gdist{curve.sdf(coords_f).gradient_signed_distance()};
+
+		gdist.distance.value = std::abs(gdist.distance.value) * (renderer_dx_geometries.is_inside(coords_f) ? -1.f : 1.f);
 			
 		const auto sample_gdist{utils::graphics::sdf::debug_sample_gradient_sdf(gdist            )};
 		const auto sample_lit  {gsdf_helpers::apply_light            (coords_f, gdist, light, 8.f)};
-
+		
 		image_gsdf[index] = sample_gdist;
 		image_lit [index] = sample_lit;
 		});
