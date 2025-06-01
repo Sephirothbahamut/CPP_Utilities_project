2ca1e9e671ec70ffeed41b29d881b38edb6b2818
sephirothbahamut <sephirothbahamut@live.it>
2025-06-01 05:01:26 +0200

    power outage backup

--- quick_tests.cpp
+++ quick_tests.cpp
@@ -6,167 +6,365 @@
 #include "Library/include/utils/containers/aggregate_regions.h"
 
 
-template <typename T> utils::observer_ptr<const T> observer_for_optional(const std::optional<      T>& opt) { return opt.has_value() ? std::addressof(*opt) : nullptr; }
-template <typename T> utils::observer_ptr<const T> observer_for_optional(      std::optional<const T>& opt) { return opt.has_value() ? std::addressof(*opt) : nullptr; }
-template <typename T> utils::observer_ptr<      T> observer_for_optional(      std::optional<      T>& opt) { return opt.has_value() ? std::addressof(*opt) : nullptr; }
-
-struct region
+namespace utils::beta
 	{
-	size_t begin{0};
-	size_t count{1};
-	utils_gpu_available inline constexpr size_t end() const noexcept { return begin + count; }
-
-	struct create : utils::oop::non_constructible
+	struct region
 		{
-		inline static constexpr region full_range() noexcept { return region{0, std::numeric_limits<size_t>::max()}; }
-		inline static constexpr region from_to(size_t start, size_t end) noexcept
+		size_t begin{0};
+		size_t count{1};
+		utils_gpu_available inline constexpr size_t end() const noexcept { return begin + count; }
+
+		struct create : utils::oop::non_constructible
 			{
-			assert(end > start);
-			return region{start, end - start};
-			}
-		inline static constexpr region from(size_t start) noexcept { return from_to(start, std::numeric_limits<size_t>::max()); }
+			inline static constexpr region full_range() noexcept { return region{0, std::numeric_limits<size_t>::max()}; }
+			inline static constexpr region from_to(size_t start, size_t end) noexcept
+				{
+				assert(end > start);
+				return region{start, end - start};
+				}
+			inline static constexpr region from(size_t start) noexcept { return from_to(start, std::numeric_limits<size_t>::max()); }
+			};
+
+		bool operator==(const region& other) const noexcept = default;
 		};
 
-	bool operator==(const region& other) const noexcept = default;
-	};
 
 
 
 
 
+	template <typename T>
+	class regions
+		{
+		public:
+			using value_type = T;
 
-//template <typename T>
-class regions
-	{
-	public:
-		using value_type = int;// T;
-
-		struct read_slot
-			{
-			using value_type = regions::value_type;
-			const region region;
-			utils::observer_ptr<const value_type> value_ptr;
-			};
-
-	private:
-		struct inner_slot_t
-			{
-			std::optional<value_type> value_opt{std::nullopt};
-			size_t end{std::numeric_limits<size_t>::max()};
-			};
-
-		std::vector<inner_slot_t> inner_slots;
-
-		size_t inner_slots_index_for_value_at(const size_t& index) const noexcept
-			{
-			for (size_t index_in_slots{0}; index_in_slots < inner_slots.size(); index_in_slots++)
+			struct read_slot
 				{
-				const inner_slot_t& inner_slot{inner_slots[index_in_slots]};
-				if (inner_slot.end > index) 
-					{
-					return index_in_slots;
-					}
-				}
-			std::unreachable();
-			}
-		const inner_slot_t             & inner_slot_for_value_at(const size_t& index) const noexcept { return inner_slots[inner_slots_index_for_value_at(index)]          ; }
-		/***/ inner_slot_t             & inner_slot_for_value_at(const size_t& index) /***/ noexcept { return inner_slots[inner_slots_index_for_value_at(index)]          ; }
-		/***/ std::optional<value_type>& value_at               (const size_t& index) /***/ noexcept { return inner_slot_for_value_at(index).value_opt; }
+				using value_type = regions::value_type;
+				const region region;
+				const value_type& value;
+				};
 
-
-	public:
-		const std::optional<value_type>& value_at(const size_t& index) const noexcept { return inner_slot_for_value_at(index).value_opt; }
-
-		read_slot slot_at(const size_t& index) const noexcept
-			{
-			const size_t inner_slots_index{inner_slots_index_for_value_at(index)};
-			const inner_slot_t& inner_slot{inner_slots[inner_slots_index]};
-
-			if (inner_slots_index == 0)
+		private:
+			struct inner_slot_t
 				{
-				const read_slot ret
+				value_type value;
+				size_t end{std::numeric_limits<size_t>::max()};
+				};
+			using inner_slots_t = std::vector<inner_slot_t>;
+
+			inner_slots_t inner_slots;
+
+			size_t inner_slots_index_for_value_at(const size_t& index, const size_t& inner_slots_starting_index = 0) const noexcept
+				{
+				for (size_t index_in_slots{inner_slots_starting_index}; index_in_slots < inner_slots.size(); index_in_slots++)
 					{
-					.region
+					const inner_slot_t& inner_slot{inner_slots[index_in_slots]};
+					if (inner_slot.end > index) 
 						{
-						.begin{0},
-						.count{inner_slot.end}
-						},
-					.value_ptr{observer_for_optional(inner_slot.value_opt)}
+						return index_in_slots;
+						}
+					}
+				std::unreachable();
+				}
+			const inner_slot_t& inner_slot_for_value_at(const size_t& index) const noexcept { return inner_slots[inner_slots_index_for_value_at(index)]; }
+			/***/ inner_slot_t& inner_slot_for_value_at(const size_t& index) /***/ noexcept { return inner_slots[inner_slots_index_for_value_at(index)]; }
+			/***/ value_type  & value_at               (const size_t& index) /***/ noexcept { return inner_slot_for_value_at(index).value; }
+
+
+		public:
+			const value_type& value_at(const size_t& index) const noexcept { return inner_slot_for_value_at(index).value; }
+
+			read_slot slot_at(const size_t& index) const noexcept
+				{
+				const size_t inner_slots_index{inner_slots_index_for_value_at(index)};
+				const inner_slot_t& inner_slot{inner_slots[inner_slots_index]};
+
+				if (inner_slots_index == 0)
+					{
+					const read_slot ret
+						{
+						.region
+							{
+							.begin{0},
+							.count{inner_slot.end}
+							},
+						.value{inner_slot.value}
+						};
+					return ret;
+					}
+				else
+					{
+					const inner_slot_t& previous_inner_slot{inner_slots[inner_slots_index - 1]};
+					const read_slot ret
+						{
+						.region{region::create::from_to(previous_inner_slot.end, inner_slot.end)},
+						.value{inner_slot.value}
+						};
+					return ret;
+					}
+				}
+
+		public:
+			regions(const value_type& starting_value) noexcept : 
+				inner_slots{inner_slot_t
+					{
+					.value{starting_value},
+					.end{std::numeric_limits<size_t>::max()}
+					}}
+				{}
+
+			void add(const value_type& new_value, const region& in_region)
+				{
+				const size_t index_of_slot_starting_before_or_at_begin{inner_slots_index_for_value_at(in_region.begin)};
+				inner_slot_t& slot_starting_before_or_at_begin{inner_slots[index_of_slot_starting_before_or_at_begin]};
+
+				if (slot_starting_before_or_at_begin.value != new_value)
+					{
+					const size_t begin_of_slot_starting_before_or_at_begin{index_of_slot_starting_before_or_at_begin == 0 ? static_cast<size_t>(0) : inner_slots[index_of_slot_starting_before_or_at_begin - 1].end};
+					if (begin_of_slot_starting_before_or_at_begin == in_region.begin)
+						{
+						if (slot_starting_before_or_at_begin.end > in_region.end())
+							{
+							const inner_slot_t new_slot
+								{
+								.value{new_value},
+								.end{in_region.end()}
+								};
+
+							inner_slots.insert(inner_slots.begin() + index_of_slot_starting_before_or_at_begin, new_slot);
+							return;
+							}
+						else
+							{
+							slot_starting_before_or_at_begin.value = new_value;
+							slot_starting_before_or_at_begin.end = in_region.end();
+							add_inner_guaranteed_previous_contains_same_value(index_of_slot_starting_before_or_at_begin, new_value, in_region);
+							return;
+							}
+						}
+					else if (slot_starting_before_or_at_begin.end > in_region.end())
+						{
+						const inner_slot_t new_slot
+							{
+							.value{new_value},
+							.end{in_region.end()}
+							};
+						const inner_slot_t replicate_previous_after_new
+							{
+							.value{slot_starting_before_or_at_begin.value},
+							.end{slot_starting_before_or_at_begin.end}
+							};
+
+						slot_starting_before_or_at_begin.end = in_region.begin;
+
+						inner_slots.insert(inner_slots.begin() + index_of_slot_starting_before_or_at_begin + 1, new_slot);
+						inner_slots.insert(inner_slots.begin() + index_of_slot_starting_before_or_at_begin + 2, replicate_previous_after_new);
+						return;
+						}
+					else //if (slot_starting_before_or_at_begin.end <= in_region.end() %% begin_of_... != in_region.begin) 
+						{
+						slot_starting_before_or_at_begin.end = in_region.begin;
+						add(new_value, in_region);
+						return;
+						}
+					}
+
+				if (slot_starting_before_or_at_begin.value == new_value)
+					{
+					add_inner_guaranteed_previous_contains_same_value(index_of_slot_starting_before_or_at_begin, new_value, in_region);
+					}
+				}
+	
+			class const_forward_iterator
+				{
+				private:
+					friend class regions;
+
+					using difference_type = std::ptrdiff_t;
+					using value_type = regions::value_type;
+
+					size_t begin{0};
+					inner_slots_t::const_iterator inner_it;
+
+					const_forward_iterator(size_t begin, inner_slots_t::const_iterator inner_it) :
+						begin   {begin   },
+						inner_it{inner_it}
+						{
+						}
+
+				public:
+					const_forward_iterator& operator++() noexcept
+						{
+						begin = inner_it->end;
+						++inner_it;
+						return *this;
+						}
+					const_forward_iterator operator++(int) noexcept
+						{
+						auto tmp{*this};
+						++(*this);
+						return tmp;
+						}
+
+					bool operator==(const const_forward_iterator& other) const noexcept
+						{
+						return inner_it == other.inner_it;
+						}
+
+					const read_slot operator*() const noexcept 
+						{
+						const read_slot ret
+							{
+							.region{region()},
+							.value {value ()}
+							};
+						return ret; 
+						}
+
+					const value_type& value() const noexcept { return inner_it->value; }
+
+					const region region() const noexcept
+						{
+						const auto ret{utils::beta::region::create::from_to(begin, inner_it->end)};
+						return ret;
+						}
+
+					bool is_end() const noexcept 
+						{
+						const bool ret{begin == std::numeric_limits<size_t>::max()};
+						return ret;
+						}
+				};
+
+			const_forward_iterator begin() const noexcept
+				{
+				const const_forward_iterator ret
+					{
+					static_cast<size_t>(0),
+					inner_slots.begin()
 					};
 				return ret;
 				}
-			else
+			const_forward_iterator end() const noexcept
 				{
-				const inner_slot_t& previous_inner_slot{inner_slots[inner_slots_index - 1]};
-				const read_slot ret
+				const const_forward_iterator ret
 					{
-					.region{region::create::from_to(previous_inner_slot.end, inner_slot.end)},
-					.value_ptr{observer_for_optional(inner_slot.value_opt)}
+					std::numeric_limits<size_t>::max(),
+					inner_slots.end()
 					};
 				return ret;
 				}
-			}
 
-	public:
-		regions() noexcept : 
-			inner_slots{inner_slot_t
-				{
-				.value_opt{std::nullopt},
-				.end{std::numeric_limits<size_t>::max()}
-				}}
-			{}
-
-		void add(const value_type& new_value, const region& in_region) { add(std::optional<value_type>{new_value}, in_region); }
-		void erase(const region& in_region) { add(std::nullopt, in_region); }
-		void add(const std::optional<value_type>& new_value_opt, const region& in_region)
-			{
-			const size_t index_of_slot_starting_before_or_at_begin{inner_slots_index_for_value_at(in_region.begin)};
-			inner_slot_t& slot_starting_before_or_at_begin{inner_slots[index_of_slot_starting_before_or_at_begin]};
-
-			if (slot_starting_before_or_at_begin.value_opt == new_value_opt)
-				{
-				if (slot_starting_before_or_at_begin.end >= in_region.end()) 
-					{ 
-					return; 
-					}
-
-				//Index guaranteed to be different because otherwise we'd have entered the above if branch.
-				const size_t index_of_slot_starting_before_or_at_end{inner_slots_index_for_value_at(in_region.end())};
-				inner_slot_t& slot_starting_before_or_at_end{inner_slots[index_of_slot_starting_before_or_at_end]};
-
-				if (slot_starting_before_or_at_end.value_opt == new_value_opt)
+			private:
+				void add_inner_guaranteed_previous_contains_same_value(size_t index_of_slot_starting_before_or_at_begin, const value_type& new_value, const region& in_region)
 					{
-					//Next slot has same value as previous slot, and anything in between was different. Erase everything in between and next, extend previous to the end of next
-					const size_t new_end{slot_starting_before_or_at_end.end};
-					slot_starting_before_or_at_begin.end = new_end;
+					inner_slot_t& slot_starting_before_or_at_begin{inner_slots[index_of_slot_starting_before_or_at_begin]};
 
-					const auto erase_from{inner_slots.begin() + index_of_slot_starting_before_or_at_begin + 1};
-					const auto erase_to  {inner_slots.begin() + index_of_slot_starting_before_or_at_end   + 1};
-					inner_slots.erase(erase_from, erase_to);
-					return;
+					if (slot_starting_before_or_at_begin.end >= in_region.end())
+						{
+						return;
+						}
+
+					//Index guaranteed to be different because otherwise we'd have entered the above if branch.
+					const size_t index_of_slot_starting_before_or_at_end{inner_slots_index_for_value_at(in_region.end(), index_of_slot_starting_before_or_at_begin)};
+					inner_slot_t& slot_starting_before_or_at_end{inner_slots[index_of_slot_starting_before_or_at_end]};
+
+					if (slot_starting_before_or_at_end.value == new_value)
+						{
+						//Next slot has same value as previous slot, and anything in between was different. Erase everything in between and next, extend previous to the end of next
+						const size_t new_end{slot_starting_before_or_at_end.end};
+						slot_starting_before_or_at_begin.end = new_end;
+
+						const auto erase_from{inner_slots.begin() + index_of_slot_starting_before_or_at_begin + 1};
+						const auto erase_to{inner_slots.begin() + index_of_slot_starting_before_or_at_end + 1};
+						inner_slots.erase(erase_from, erase_to);
+						return;
+						}
+					else if (index_of_slot_starting_before_or_at_end == (index_of_slot_starting_before_or_at_begin + 1))
+						{
+						//The first slot that ends after new region's end is right after the last slot that begins before new region's begin.
+						//No in-between regions to remove, just move the end value.
+						slot_starting_before_or_at_begin.end = in_region.end();
+						return;
+						}
+					else if (index_of_slot_starting_before_or_at_end == (index_of_slot_starting_before_or_at_begin + 1))
+						{
+						//There are in-between regions with different values to erase
+						slot_starting_before_or_at_begin.end = in_region.end();
+
+						const auto erase_from{inner_slots.begin() + index_of_slot_starting_before_or_at_begin + 1};
+						const auto erase_to{inner_slots.begin() + index_of_slot_starting_before_or_at_end};
+						inner_slots.erase(erase_from, erase_to);
+						return;
+						}
+					std::unreachable();
 					}
-				else if (index_of_slot_starting_before_or_at_end == (index_of_slot_starting_before_or_at_begin + 1))
-					{
-					//The first slot that ends after new region's end is right after the last slot that begins before new region's begin.
-					//No in-between regions to remove, just move the end value.
-					slot_starting_before_or_at_begin.end = in_region.end();
-					return;
-					}
-				else if (index_of_slot_starting_before_or_at_end == (index_of_slot_starting_before_or_at_begin + 1))
-					{
-					//There are in-between regions with different values to erase
-					slot_starting_before_or_at_begin.end = in_region.end();
-
-					const auto erase_from{inner_slots.begin() + index_of_slot_starting_before_or_at_begin + 1};
-					const auto erase_to  {inner_slots.begin() + index_of_slot_starting_before_or_at_end};
-					inner_slots.erase(erase_from, erase_to);
-					return;
-					}
-				}
-			}
-	};
+		};
+	}
 
 
+
+void new_regions()
+	{
+	utils::beta::regions<float> regions{std::numeric_limits<float>::quiet_NaN()};
+
+	regions.add(0.f, utils::beta::region::create::from_to(10, 20));
+
+	for (const auto& slot : regions)
+		{
+		std::cout << "[" << slot.region.begin << "-" << slot.region.end() << "): " << slot.value << "\n";
+		}
+	std::cout << "\n________________________________\n" << std::endl;
+
+	regions.add(1.f, utils::beta::region::create::from_to(20, 25));
+
+	for (const auto& slot : regions)
+		{
+		std::cout << "[" << slot.region.begin << "-" << slot.region.end() << "): " << slot.value << "\n";
+		}
+	std::cout << "\n________________________________\n" << std::endl;
+
+	regions.add(2.f, utils::beta::region::create::from_to(8, 10));
+
+	for (const auto& slot : regions)
+		{
+		std::cout << "[" << slot.region.begin << "-" << slot.region.end() << "): " << slot.value << "\n";
+		}
+
+	std::cout << "\n________________________________\n" << std::endl;
+	regions.add(3.f, utils::beta::region::create::from_to(6, 15));
+
+	for (const auto& slot : regions)
+		{
+		std::cout << "[" << slot.region.begin << "-" << slot.region.end() << "): " << slot.value << "\n";
+		}
+
+	std::cout << "\n________________________________\n" << std::endl;
+	regions.add(4.f, utils::beta::region::create::from_to(6, 20));
+
+	for (const auto& slot : regions)
+		{
+		std::cout << "[" << slot.region.begin << "-" << slot.region.end() << "): " << slot.value << "\n";
+		}
+
+	std::cout << "\n________________________________\n" << std::endl;
+	regions.add(5.f, utils::beta::region::create::from_to(19, std::numeric_limits<size_t>::max()));
+
+	for (const auto& slot : regions)
+		{
+		std::cout << "[" << slot.region.begin << "-" << slot.region.end() << "): " << slot.value << "\n";
+		}
+
+	std::cout << "\n________________________________\n" << std::endl;
+	regions.add(4.f, utils::beta::region::create::from_to(19, 20));
+
+	for (const auto& slot : regions)
+		{
+		std::cout << "[" << slot.region.begin << "-" << slot.region.end() << "): " << slot.value << "\n";
+		}
+	}
 
 
 
@@ -361,6 +559,8 @@ void regions()
 
 int main()
 	{
-	aggregate_and_regions();
-	regions();
+	//aggregate_and_regions();
+	//regions();
+
+	new_regions();
 	}
